19dd8662e0809d076c1ca695927bc3e4fbc13e32
Nicola <nicola@harrisonsworld.com>
2021-07-07 23:16:39 +0100

    Level Select and Arrow button navigation works.
    All buttons lead to correct place.

--- math-mania/src/webpages/selectLevel.jsx
+++ math-mania/src/webpages/selectLevel.jsx
@@ -4,16 +4,38 @@ import "../App.css";
 
 import LevelSelectTitle from '../Assets/LevelSelectTitle.png';
 import Arrow from '../Assets/arrow.png';
+import Level0CLloud from '../Assets/Level0Cloud.png';
+import Level1Cloud from '../Assets/Level1Cloud.png';
+import Level2Cloud from '../Assets/Level2Cloud.png';
 
 const SelectLevel = () => {
     return (
         <div>
             <a href="/MainAppPage">
-            <img src={Arrow} width="10%" className="arrowPadding" />
+                <img src={Arrow} width="10%" className="arrowPadding" />
             </a>
             <div className="App">
                 <div className="levelSelectTitle">
                     <img src={LevelSelectTitle} />
+                </div>
+                <div>
+                    <div className="cloudPadding">
+                        <a href="/Level0">
+                            <img src={Level0CLloud} />
+                        </a>
+                    </div>
+
+                    <div className="cloudPadding">
+                        <a href="/Level1">
+                            <img src={Level1Cloud} />
+                        </a>
+                    </div>
+
+                    <div className="cloudPadding">
+                        <a href="/Level2">
+                            <img src={Level2Cloud} />
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
