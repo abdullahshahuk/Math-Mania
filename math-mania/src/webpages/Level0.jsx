803c224c4b91d2a52b4b3622e8197d2c23d751e5
ABZZDEV <ABZZDEV@GMAIL.COM>
2021-07-07 23:33:22 +0100

    pulling window url

--- math-mania/src/webpages/Level0.jsx
+++ math-mania/src/webpages/Level0.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
+
+
 const Level0 = () => {
     return(
         <div>
             <h3>Welcome to the React Router Tutorial</h3>
             <small>Level 0</small>
+            <br></br>
+            "The full URL of this page is: "{window.location.href}
         </div>
+        
     );
 };
 
