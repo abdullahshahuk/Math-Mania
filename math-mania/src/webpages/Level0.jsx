56ddba9251599cc5dff4c385e5dd31fbf8ddb512
Nicola <nicola@harrisonsworld.com>
2021-07-07 21:31:37 +0100

    Full React app and Router (Page links) Working
    The empty react app with all the links to the correct pages.

---
+++ math-mania/src/webpages/Level0.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Level0 = () => {
+    return(
+        <div>
+            <h3>Welcome to the React Router Tutorial</h3>
+            <small>Level 0</small>
+        </div>
+    );
+};
+
+export default Level0;
---
+++ math-mania/src/webpages/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const MainAppPage = () => {
+    return (
+        <div>
+            <h3>Welcome to the React Router Tutorial</h3>
+            <small>Main Page</small>
+        </div>
+    );
+}
+
+export default MainAppPage;
