d5ac51a7d80bd785f106908c3890041ee8cc7fd4
Nicola <nicola@harrisonsworld.com>
2021-07-07 23:01:22 +0100

    Main Page Done, and Linked to Level Select

--- math-mania/src/webpages/index.jsx
+++ math-mania/src/webpages/index.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import "../App.css";
+
+import MathMania from '../Assets/MathMania.png';
+import Rocket from '../Assets/rocket.png';
+
+import { Card } from 'reactstrap';
+
 
 
 const MainAppPage = () => {
     return (
-        <div>
-            <h3>Welcome to the React Router Tutorial</h3>
-            <small>Main Page</small>
+        <div className="App">
+            <div className="mathManiaPadding">
+                <img src={MathMania} />
+            </div>
+            <div className="mathManiaButtonPadding">
+            <a href="/selectLevel">
+                <Card style={{ borderRadius:"100%" , backgroundColor:"#020269"}}>
+                        <img src={Rocket} width="%" />
+                </Card>
+                </a>
+            </div>
+            <div className="App pressStart">
+                <h3>PRESS TO START!</h3>
+            </div>
         </div>
     );
 }
--- math-mania/src/webpages/selectLevel.jsx
+++ math-mania/src/webpages/selectLevel.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+// import Level0 from './Level0';
+import "../App.css";
+
+import LevelSelectTitle from '../Assets/LevelSelectTitle.png';
+import Arrow from '../Assets/arrow.png';
 
 const SelectLevel = () => {
     return (
         <div>
-            <h3>Welcome to the React Router Tutorial</h3>
-            <small>Select Level</small>
+            <a href="/MainAppPage">
+            <img src={Arrow} width="10%" className="arrowPadding" />
+            </a>
+            <div className="App">
+                <div className="levelSelectTitle">
+                    <img src={LevelSelectTitle} />
+                </div>
+            </div>
         </div>
     );
 };
 
-export default SelectLevel; 
+export default SelectLevel;
